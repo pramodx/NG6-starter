9a13fe3bd0ec4f4b02ab02986262b7e4a05b6462
Pramod Nair <pramod.nair@emprevo.com>
2017-05-28 12:04:01 +1000

    sap menu done

--- client/app/services/agents/agents.js
+++ client/app/services/agents/agents.js
@@ -3,10 +3,13 @@ const agentsService = ($http) => {
     const baseUrl = 'https://api.ratemyagent.com.au/autosearch/agents';
     let data = [];
 
-    const getAgents = (param) => {
-        return $http.get(`${baseUrl}?SearchTerm=${param}`)
-            .then(response => data = response.data);
-
+    const getAgents = (searchTerm) => {
+        return $http({
+            url: baseUrl,
+            method: 'GET',
+            params: {SearchTerm: searchTerm}
+        }).then(response => data = response.data);
+T
     }
 
     return { getAgents }
